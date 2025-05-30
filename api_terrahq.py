47854a7f2f30ef48da273fa47f3e0a62f22773d3
PacoFletcher <pacofletcher@gmail.com>
2025-05-30 13:19:58 +0200

    primera_api

---
+++ api_terrahq.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from flask import Flask, jsonify,request
+from flask import render_template
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, mean_absolute_percentage_error
+import os
+from sklearn.ensemble import RandomForestRegressor
+import numpy as np
+import pickle
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>API para TERRAHQ</h1><p>Ésta es una API.</p>"
